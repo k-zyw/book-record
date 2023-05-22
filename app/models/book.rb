1b4282add1c7959fa6eab786ba1bec96a141a9f0
k-zyw <jianwuliyuan448@gmail.com>
2023-05-22 11:08:52 +0900

    bookモデル、バリデーション

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,9 @@
 class Book < ApplicationRecord
+  validates :category_id,     presence: true
+  validates :title,           presence: true
+  validates :author,          presence: true
+  validates :description,     presence: true
+  validates :image,           presence: true
+  
+  belongs_to :user
 end
